fe6cc87c5dff529081725a03fae08fb3b6f0e0f9
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-09-03 00:54:13 +0400

    Create MixinEntityPlayerSP.java

---
+++ src/main/java/de/vepexlegit/autorespawn/mixins/MixinEntityPlayerSP.java
@@ -0,0 +1,22 @@
+package de.vepexlegit.autorespawn.mixins;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EntityPlayerSP.class)
+public class MixinEntityPlayerSP {
+
+    @Shadow private Minecraft mc = Minecraft.getMinecraft();
+
+    @Inject(method = "onUpdate", at = @At("HEAD"))
+    private void onUpdate(final CallbackInfo ci) {
+        if (!mc.thePlayer.isEntityAlive()) {
+            mc.thePlayer.respawnPlayer();
+        }
+    }
+}
